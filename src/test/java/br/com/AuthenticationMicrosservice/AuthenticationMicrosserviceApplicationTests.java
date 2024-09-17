bdccb33f5fdaba5325b5930582f67d8d12a6f203
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-09-17 20:20:11 -0300

    initialized project

---
+++ src/test/java/br/com/AuthenticationMicrosservice/AuthenticationMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AuthenticationMicrosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
