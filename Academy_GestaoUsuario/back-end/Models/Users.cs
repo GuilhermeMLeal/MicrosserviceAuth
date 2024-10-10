e242af34f8b36ced33fd35b0609f3421a241d596
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-10-10 20:32:07 -0300

    feat(Users): added base solution to user management

---
+++ Academy_GestaoUsuario/back-end/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_end.Models
+{
+    public class Users
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(255, ErrorMessage = "O nome deve ter no máximo 255 caracteres.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "O registro é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O registro deve ter no máximo 50 caracteres.")]
+        public required string Registro { get; set; }
+
+        [StringLength(100, ErrorMessage = "O curso deve ter no máximo 100 caracteres.")]
+        public string? Course { get; set; }
+
+        public int EntryYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "A classe deve ter no máximo 50 caracteres.")]
+        public string? Class { get; set; }
+
+        [StringLength(50, ErrorMessage = "A classe deve ter no máximo 50 caracteres.")]
+        public DateOnly Birthday { get; set; }
+    }
+}
---
+++ Academy_GestaoUsuario/back-end/Repository/UserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace back_end.Repository
+{
+    public class UserRepository
+    {
+    }
+}
