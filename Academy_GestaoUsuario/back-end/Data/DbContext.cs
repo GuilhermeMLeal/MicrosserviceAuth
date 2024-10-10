82448fe76b644d1785ef2b4886e3326ba74b01d6
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-10-10 20:54:13 -0300

    feat(DbContext): context archive: "DbContext" created

---
+++ Academy_GestaoUsuario/back-end/Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_end
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
